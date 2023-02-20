53627a8048a08fb2d8237f87a8aaaf436853ac97
Vilas Chavhan <chavhanvilas02@gmail.com>
2023-02-20 18:08:12 +0530

    changed ui of cart page

--- src/Components/Products.jsx
+++ src/Components/Products.jsx
@@ -35,6 +35,10 @@ export default function Products() {
 					<Shimmer />
 					<Shimmer />
 					<Shimmer />
+					<Shimmer />
+					<Shimmer />
+					<Shimmer />
+					<Shimmer />
 				</div>
 			)}
 		</div>
--- src/Pages/Cart.jsx
+++ src/Pages/Cart.jsx
@@ -1,41 +1,93 @@
 import { useDispatch, useSelector } from 'react-redux';
-import { remove,increaseQty,decreaseQty } from '../Features/CartSlice';
+import { remove, increaseQty, decreaseQty } from '../Features/CartSlice';
 export default function Cart() {
 	const cartList = useSelector((state) => state.cart);
 	const dispatch = useDispatch();
+	
 	const handleRemove = (productId) => {
 		dispatch(remove(productId));
 	};
 
+	const getTotal = () =>{
+		let qty = 0;
+		let total = 0;
+		cartList.map((product) => {
+			qty = product.qty
+			total = product.price * qty;
+			console.log(total)
+		})
+		return {total};
+	}
 	return (
 		<div className="px-5 py-5 flex flex-col bg-gray-100 min-h-[90vh]">
 			<h1 className="text-2xl font-bold">Cart</h1>
-			{cartList.length !== 0 ? (
-				cartList &&
-				cartList.map((product) => (
-					<div className="bg-white flex justify-between items-center mt-5 p-5" key={product.id}>
-						<img src={product.image} alt="product" className="h-28" />
-						<h4 className="font-bold">{product.title}</h4>
-						<h5 className="font-bold">{ product.price * product.qty}</h5>
-						<div className='flex space-x-3'>
-							<button className='font-bold text-4xl' onClick={() => dispatch(increaseQty(product.id))}>+</button>
-						<h5  className="font-bold">{product.qty}</h5>
-						<button className='font-bold text-4xl' onClick={() => dispatch(decreaseQty(product.id))}>-</button>
-						</div>
-						<button
-							onClick={() => handleRemove(product.id)}
-							className="bg-blue-400 px-8 py-2 rounded-full
+			<table className="mt-5 table-auto">
+				<thead className="bg-blue-200 text-black">
+					<th className="p-5 ">Product Image</th>
+					<th>Product Title</th>
+					<th>Product Price</th>
+					<th>Product Quantity</th>
+					<th>Remove</th>
+				</thead>
+				<tbody>
+					{cartList.length !== 0 ? (
+						cartList.map((product) => (
+							<tr className="text-start" key={product.id}>
+								<td className="py-5">
+									<img src={product.image} alt="product" className="h-28" />
+								</td>
+								<td>
+									<h4 className="font-bold">{product.title}</h4>
+								</td>
+								<td>
+									<h5 className="font-bold"> &#36;{product.price * product.qty} </h5>
+								</td>
+								<td>
+									<div className="flex items-center justify-center space-x-3">
+										<button
+											className="font-bold text-2xl pb-1 bg-blue-400 hover:bg-blue-500 px-7 py-[1px] text-center rounded-full"
+											onClick={() => dispatch(increaseQty(product.id))}
+										>
+											+
+										</button>
+										<h5 className="font-bold my-auto">{product.qty}</h5>
+										<button
+											className="font-bold text-2xl pb-1 bg-blue-400  hover:bg-blue-500 px-8 py-[1px] text-center rounded-full"
+											onClick={() => dispatch(decreaseQty(product.id))}
+										>
+											-
+										</button>
+									</div>
+								</td>
+								<td>
+									<button
+										onClick={() => handleRemove(product.id)}
+										className="bg-blue-400 hover:bg-blue-500 px-8 py-2 rounded-full
                             mt-4 font-bold"
-						>
-							Remove
-						</button>
-					</div>
-				))
-			) : (
-				<div className="flex items-center justify-center mt-5">
-					<h1 className="text-2xl font-semibold">Cart is empty</h1>
+									>
+										Remove
+									</button>
+								</td>
+							</tr>
+							// <div className="bg-white flex justify-between items-center mt-5 p-5" key={product.id}>
+
+							// </div>
+						))
+					) : (
+						<div className="flex items-center justify-center mt-5 text-center">
+							<h1 className="text-2xl font-semibold text-center">Cart is empty</h1>
+						</div>
+					)}
+				</tbody>
+			</table>
+			{cartList.length >= 1 ? (
+					<div className="float-right ml-auto md:mr-10 flex items-center flex-col">
+						<h5 className='font-bold text-2xl'>Total : &#36;<span>{getTotal().total}</span></h5>
+					<button className="bg-blue-400 hover:bg-blue-500 px-8 py-2 rounded-full
+                            mt-4 font-bold">Place Order</button>
 				</div>
-			)}
+			) : ''}
+		
 		</div>
 	);
 }
